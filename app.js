282d508eaf9694472a9165ebc081a25ef750c252
tlg <tulllga@gmail.com>
2023-10-31 13:11:14 +0800

    shoog shiddeh bolgoh

--- app.js
+++ app.js
@@ -13,15 +13,25 @@ let roundScore = 0;
 
 // Shoonii buusan taliig hadgalah function
 
-let dice = Math.ceil(Math.random()*6);
-
 //<div class="player-score" id="score-0">43</div>
-window.document.querySelector('#score-0').textContent = 0;
-window.document.querySelector('#score-1').textContent = 0;
+window.document.getElementById('score-0').textContent = 0;
+window.document.getElementById('score-1').textContent = 0;
 
-window.document.querySelector('#current-0').textContent = 0;
-window.document.querySelector('#current-1').textContent = 0;
+window.document.getElementById('current-0').textContent = 0;
+window.document.getElementById('current-1').textContent = 0;
 
-document.querySelector(".dice").style.display = "none";
+let diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
-console.log('Shoo: ',dice);
+document.querySelector(".btn-roll").addEventListener('click', function(){
+    let diceNumber = Math.ceil(Math.random()*6);
+    console.log(diceNumber);
+
+    document.querySelector(".dice").style.display = "block";
+    document.querySelector(".dice").src = 'dice-' + diceNumber + '.png';
+    
+})
+
+
+
+
