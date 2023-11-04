49e9bcc3dd2552d836a881ecd570a253b540c1fb
tulga <tulllga@gmail.com>
2023-11-05 00:39:52 +0800

    new game

--- app.js
+++ app.js
@@ -1,24 +1,57 @@
-// Тоглогчийг ээлжийг хадгалах хувьсагч
-// player1 = 0, player2 = 1;
-let activePlayer = 0;
+//global huvisagchid
 
-// Toglogchdiin tsugluulsan onoog hadgalah huvisagch
-let srores = [0,0];
+//idvehtei toglogch
+let activePlayer;
 
-// Toglogchiin onoog hadgalah
-let roundtScore = 0;
-//let roundtScore = 0;
+//tsugluulsan onoo
+let scores;
 
+//togloltiin onoo
+let roundtScore;
 
-// Shoonii buusan taliig hadgalah function
-window.document.getElementById('score-0').textContent = 0;
-window.document.getElementById('score-1').textContent = 0;
-
-window.document.getElementById('current-0').textContent = 0;
-window.document.getElementById('current-1').textContent = 0;
-
+//shoonii zurgiig uzuuleh emlentiig domoos avchirna
 let diceDom = document.querySelector(".dice");
-diceDom.style.display = "none";
+
+//togloom ehluuleh
+initGames();
+
+//togloom ehleh
+function initGames(){
+    // huvisagchdiin utgiig 0 bolgoh
+
+    activePlayer = 0;
+
+    // Toglogchdiin tsugluulsan onoog hadgalah huvisagch
+    srores = [0,0];
+
+    // Toglogchiin onoog hadgalah
+    roundtScore = 0;
+    //let roundtScore = 0;
+
+
+    // Shoonii buusan taliig hadgalah function
+    window.document.getElementById('score-0').textContent = 0;
+    window.document.getElementById('score-1').textContent = 0;
+
+    window.document.getElementById('current-0').textContent = 0;
+    window.document.getElementById('current-1').textContent = 0;
+
+    //toglochdiin neriin haruulah
+    document.getElementById('name-0').textContent = "Player 1"
+    document.getElementById('name-1').textContent = "Player 2"
+
+    //new game button daragdah ved winner effect arilah yostoi
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('winner');
+
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    
+    
+    document.querySelector('.player-0-panel').classList.add('active');
+
+    diceDom.style.display = "none";
+}
 
 //Shoog shideh event listener
 document.querySelector(".btn-roll").addEventListener('click', function(){
@@ -87,9 +120,9 @@ function swichToNextPlayer(){
 }
 
 //new game button eventlister
-document.querySelector('.btn-new').addEventListener('click', function(){
-    alert(111);
-})
+document.querySelector('.btn-new').addEventListener('click', initGames)
+
+
 
 
 
