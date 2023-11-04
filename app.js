950d901777c88c264a83bd2abb5f57f16d2b5a1b
tulga <tulllga@gmail.com>
2023-11-04 23:23:04 +0800

    toglogchiin eeljiig solih

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 // Тоглогчийг ээлжийг хадгалах хувьсагч
 // player1 = 0, player2 = 1;
-
-let activePlayer = 1;
+let activePlayer = 0;
 
 // Toglogchdiin tsugluulsan onoog hadgalah huvisagch
-
 let srores = [0,0];
 
 // Toglogchiin onoog hadgalah
+let roundtScore = 0;
+//let roundtScore = 0;
 
-let roundScore = 0;
 
 // Shoonii buusan taliig hadgalah function
-
-//<div class="player-score" id="score-0">43</div>
 window.document.getElementById('score-0').textContent = 0;
 window.document.getElementById('score-1').textContent = 0;
 
@@ -23,13 +20,39 @@ window.document.getElementById('current-1').textContent = 0;
 let diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 
+//Shoog shideh event listener
 document.querySelector(".btn-roll").addEventListener('click', function(){
-    let diceNumber = Math.ceil(Math.random()*6);
-    console.log(diceNumber);
 
+    //1-6 dotorhi sanamsargui neg too gargaj avna
+    let diceNumber = Math.ceil(Math.random()*6);
+
+    //Shoonii zurgiig web deer gargaj irne
     document.querySelector(".dice").style.display = "block";
+
+    //Buusan sanamsargui toond hargalzah shoonii zurgiig haruulna
     document.querySelector(".dice").src = 'dice-' + diceNumber + '.png';
-    
+
+    //1s yaltai bol Toglogchiin eeljiin onoog uurchluh
+    if(diceNumber !== 1){
+        roundtScore = roundtScore + diceNumber;
+        document.getElementById('current-' + activePlayer).textContent = roundtScore;
+    }else{
+        //Toglogchiin eeljiin onoog 0 bolgono
+        document.getElementById('current-' + activePlayer).textContent = 0;
+        roundtScore = 0;
+
+        //Toglogchiin eeljiig soli
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+        //ulaan tsegiig shiljuuleh
+        document.querySelector('.player-0-panel').classList.toggle("active");
+        document.querySelector('.player-1-panel').classList.toggle("active");
+
+        //shoog tur alga bolgoh
+        diceDom.style.display = "none";
+    }
+
+
 })
 
 
